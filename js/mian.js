e0ed67c28a90c33b8c6bfa9c197f61bfedaf88a5
Bykit <731240932@qq.com>
2023-02-25 19:48:32 +0800

    JS BOM

--- js/mian.js
+++ js/mian.js
@@ -1,3 +1,1 @@
-// import * as cls from "./grammar/classnote.js"
-// import * as cls from "./grammar/basicsnotes.js"
-import * as arr from "./api/struct/studyObject.js"
+import * as arr from "./api/web/bom/studyStorage.js"
