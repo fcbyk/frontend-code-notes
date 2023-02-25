82cb9ff5f5da8923f29dcffa33439f2edf246878
Bykit <731240932@qq.com>
2023-02-25 15:09:13 +0800

    JS 内置对象初步了解

--- js/mian.js
+++ js/mian.js
@@ -1,2 +1,3 @@
 // import * as cls from "./grammar/classnote.js"
-import * as cls from "./grammar/basicsnotes.js"
+// import * as cls from "./grammar/basicsnotes.js"
+import * as arr from "./api/struct/studyObject.js"
