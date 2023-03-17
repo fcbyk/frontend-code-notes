9c19f3378251e164f9b2f01d6e886276f93a7765
Bykit <731240932@qq.com>
2023-03-17 21:28:54 +0800

    NODE http内置模块学习

--- node/mian.js
+++ node/mian.js
@@ -1,4 +1,6 @@
-const fs = require("./api/builtin/studyfs.js")
-const path = require("./api/builtin/studypath.js")
+// const fs = require("./api/builtin/studyfs.js")
+// const path = require("./api/builtin/studypath.js")
+// path.pathname();
 
-path.pathname();
+// 导入模块会自动执行模块里面的代码
+// require("./api/builtin/studyhttp.js")
