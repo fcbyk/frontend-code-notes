b5f31d7e155333ffb7af5433d19936ffe39e829f
Bykit <731240932@qq.com>
2023-06-05 20:31:38 +0800

    删除之前比较乱的的代码

--- js/mian.js
+++
@@ -1,4 +0,0 @@
-// import calculator from "./demo/calculator.js";
-import * as study from "./api/basics/studyPromise.js"
-
-study.test11()
--- vue/vue2x/vue.config.js
+++
@@ -1,5 +0,0 @@
-// https://cli.vuejs.org/config/
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
